af8db349f54188f7c328d54cb4e568deb96d009d
Laurent Peuch <cortex@worlddomination.be>
2015-02-13 14:06:53 +0100

    [enh] install login app

--- soya/urls.py
+++ soya/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'soya.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
