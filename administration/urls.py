dd6e485a4c63b66ad0359619308900581546ca3c
Laurent Peuch <cortex@worlddomination.be>
2015-02-15 16:22:14 +0100

    [enh] update form for te user

--- administration/urls.py
+++ administration/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView
 from django.contrib.auth.models import User
 
 
@@ -7,4 +8,5 @@ urlpatterns = patterns('administration.views',
     url(r'^$', 'dashboard', name='administration_dashboard'),
     url(r'^user/$', ListView.as_view(model=User, template_name='administration/user_list.haml'), name='administration_user_list'),
     url(r'^user/(?P<pk>\d+)/$', DetailView.as_view(model=User, template_name='administration/user_detail.haml'), name='administration_user_detail'),
+    url(r'^user/(?P<pk>\d+)/update/$', UpdateView.as_view(model=User, template_name='administration/user_update_form.haml', fields=['username', 'first_name', 'last_name', 'email']), name='administration_user_update'),
 )
