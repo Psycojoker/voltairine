488d54b8261f5785ddd33332253fbdd1d013ab5e
Laurent Peuch <cortex@worlddomination.be>
2015-02-15 15:57:59 +0100

    [enh] yay, point to users list

--- administration/urls.py
+++ administration/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, url
+from django.views.generic import ListView
+from django.contrib.auth.models import User
 
 
 urlpatterns = patterns('administration.views',
     url(r'^$', 'dashboard', name='administration_dashboard'),
+    url(r'^user/$', ListView.as_view(model=User, template_name='administration/user_list.haml'), name='administration_user_list'),
 )
