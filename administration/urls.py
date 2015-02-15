86345d4935e4fd8dfafc9396affc6bcd635d2245
Laurent Peuch <cortex@worlddomination.be>
2015-02-15 16:08:53 +0100

    [enh] yay, link to user detail page

--- administration/urls.py
+++ administration/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.models import User
 
 
 urlpatterns = patterns('administration.views',
     url(r'^$', 'dashboard', name='administration_dashboard'),
     url(r'^user/$', ListView.as_view(model=User, template_name='administration/user_list.haml'), name='administration_user_list'),
+    url(r'^user/(?P<pk>\d+)/$', DetailView.as_view(model=User, template_name='administration/user_detail.haml'), name='administration_user_detail'),
 )
