44ed828a253d958c040cf64e304a06e6ed5a14e6
Laurent Peuch <cortex@worlddomination.be>
2015-02-15 17:23:34 +0100

    [enh] yay, createview \o/

--- administration/urls.py
+++ administration/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import patterns, url
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.models import User
 
-from .views import UpdateUser
+from .views import UpdateUser, CreateUser
 
 
 urlpatterns = patterns('administration.views',
     url(r'^$', 'dashboard', name='administration_dashboard'),
     url(r'^user/$', ListView.as_view(model=User, template_name='administration/user_list.haml'), name='administration_user_list'),
+    url(r'^user/new/$', CreateUser.as_view(), name='administration_user_create'),
     url(r'^user/(?P<pk>\d+)/$', DetailView.as_view(model=User, template_name='administration/user_detail.haml'), name='administration_user_detail'),
     url(r'^user/(?P<pk>\d+)/update/$', UpdateUser.as_view(), name='administration_user_update'),
 )
--- administration/views.py
+++ administration/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, CreateView
 from django.contrib.auth.models import User
+
+
+class CreateUser(CreateView):
+    model=User
+    template_name='administration/user_update_form.haml'
+    fields=['username', 'password', 'is_staff', 'first_name', 'last_name', 'email']
+
+    def get_success_url(self):
+        return reverse('administration_user_detail', args=(self.object.pk,))
 
 
 class UpdateUser(UpdateView):
