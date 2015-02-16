f1f2b23e3aefc2939df8529a0a3f5c2fa4163530
Laurent Peuch <cortex@worlddomination.be>
2015-02-16 16:35:33 +0100

    [enh] link to section administration page, youhou

--- administration/urls.py
+++ administration/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.models import User
 
+from sections.models import SubSection
+
 from .views import UpdateUser, CreateUser
 
 
@@ -11,4 +13,6 @@ urlpatterns = patterns('administration.views',
     url(r'^user/new/$', CreateUser.as_view(), name='administration_user_create'),
     url(r'^user/(?P<pk>\d+)/$', DetailView.as_view(model=User, template_name='administration/user_detail.haml'), name='administration_user_detail'),
     url(r'^user/(?P<pk>\d+)/update/$', UpdateUser.as_view(), name='administration_user_update'),
+
+    url(r'^section/$', ListView.as_view(model=SubSection, template_name='administration/section_list.haml'), name='administration_section_list'),
 )
