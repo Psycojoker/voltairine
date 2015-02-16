82e263fa09f868d9d22977f835db896a46cb7850
Laurent Peuch <cortex@worlddomination.be>
2015-02-16 16:51:01 +0100

    [enh] add ordering and dunderscore unicode to section models

--- sections/models.py
+++ sections/models.py
@@ -6,10 +6,22 @@ class SubSection(models.Model):
     title = models.CharField(max_length=255)
     section = models.CharField(max_length=1, choices=((('1', 'Tournages'), ('2', 'Montages'), ('3', 'Producteurs'))))
 
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
 
 class SubSubSection(models.Model):
     title = models.CharField(max_length=255)
     subsection = models.ForeignKey(SubSection)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
 
 
 class Permissoins(models.Model):
