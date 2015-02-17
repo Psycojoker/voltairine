57c5a6e80a67c2b3872f851c2cb2d26b789f1ce1
Laurent Peuch <cortex@worlddomination.be>
2015-02-17 22:12:36 +0100

    [enh] add a base template to upload videos

--- upload_video/views.py
+++ upload_video/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def upload_video(request):
-    return HttpResponse("pouet")
+    return render(request, "upload/upload.haml", {})
