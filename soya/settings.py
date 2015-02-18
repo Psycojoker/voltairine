e13f3c2946c421abde454277ffac6e964152d644
Laurent Peuch <cortex@worlddomination.be>
2015-02-18 16:19:15 +0100

    [enh] configure project for media files

--- soya/settings.py
+++ soya/settings.py
@@ -100,6 +100,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 LOGIN_REDIRECT_URL = '/'
 
 FILE_UPLOAD_TEMP_DIR = os.path.join(BASE_DIR, 'chuncks')
