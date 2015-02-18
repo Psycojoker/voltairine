cb8a44a2f7077ea0a10570e1b22c0a1caf5c1d51
Laurent Peuch <cortex@worlddomination.be>
2015-02-18 16:21:00 +0100

    [enh] yay, add media urls

--- soya/urls.py
+++ soya/urls.py
@@ -1,9 +1,11 @@
+from django.conf.urls.static import static
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^$', 'soya.views.home_redirect', name='home_redirect'),
     url(r'^accounts/', include('authentification.urls')),
     url(r'^administration/', include('administration.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
