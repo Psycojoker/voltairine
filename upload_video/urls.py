8384acbc2d467ff0464589c11d5a0c4a6a387954
Laurent Peuch <cortex@worlddomination.be>
2015-02-18 01:23:14 +0100

    [enh] first dirty working version of advanced file upload

--- upload_video/urls.py
+++ upload_video/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
+from .views import ResumableUploadView
 
 
 urlpatterns = patterns('upload_video.views',
-    url(r'^upload/$', 'upload_video', name='upload_video'),
+    url(r'^upload/$', ResumableUploadView.as_view(), name='upload'),
+    url(r'^upload_page/$', 'upload_video', name='upload_video'),
 )
--- upload_video/views.py
+++ upload_video/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.forms import Form
+
+from resumable.fields import ResumableFileField
+from resumable.views import ResumableUploadView
+
+
+class ResumableForm(Form):
+    file = ResumableFileField(upload_url=reverse_lazy('upload'), chunks_dir="chuncks")
 
 
 def upload_video(request):
-    return render(request, "upload/upload.haml", {})
+    if request.method == "POST":
+        form = ResumableForm(request.POST)
+    else:
+        form = ResumableForm()
+
+    return render(request, "upload/upload.haml", {"form": form})
