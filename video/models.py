fbe7ea764d689b5bd8666f3c5809f6f7f00b728b
Laurent Peuch <cortex@worlddomination.be>
2015-02-19 01:42:49 +0100

    [mod] those fields can be null for now

--- video/models.py
+++ video/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class Video(models.Model):
     title = models.CharField(max_length=255)
     file_name = models.CharField(max_length=255)
-    film_name = models.CharField(max_length=255)
-    realisation = models.CharField(max_length=255)
-    production = models.CharField(max_length=255)
-    photo_direction = models.CharField(max_length=255)
-    observations = models.TextField()
+    film_name = models.CharField(max_length=255, null=True, blank=True)
+    realisation = models.CharField(max_length=255, null=True, blank=True)
+    production = models.CharField(max_length=255, null=True, blank=True)
+    photo_direction = models.CharField(max_length=255, null=True, blank=True)
+    observations = models.TextField(null=True, blank=True)
 
     # montage_date = models.DateField(null=True)
     # montage_version = models.CharField(max_length=255)
