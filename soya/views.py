265405beea1d4e1a17046ae297a48a129a16b9f8
Laurent Peuch <cortex@worlddomination.be>
2015-02-20 23:52:37 +0100

    [mod] let's use staff and not admin users for administration

--- soya/views.py
+++ soya/views.py
@@ -3,6 +3,6 @@ from django.core.urlresolvers import reverse
 
 
 def home_redirect(request):
-    if request.user.is_superuser:
+    if request.user.is_staff:
         return HttpResponseRedirect(reverse('administration_dashboard'))
     return HttpResponseRedirect(reverse('login'))
