3a637f0faa4931caade99f88af55d070fdc161ae
Laurent Peuch <cortex@worlddomination.be>
2015-02-21 19:58:46 +0100

    [enh] redirect non staff user to their dashboard

--- soya/views.py
+++ soya/views.py
@@ -5,4 +5,6 @@ from django.core.urlresolvers import reverse
 def home_redirect(request):
     if request.user.is_staff:
         return HttpResponseRedirect(reverse('administration_dashboard'))
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('user_dashboard'))
     return HttpResponseRedirect(reverse('login'))
