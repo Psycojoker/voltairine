d27ec4fe8e11db32969f14bcb1abd834e4259527
Laurent Peuch <cortex@worlddomination.be>
2015-02-21 20:11:25 +0100

    [fix] oups, forgot to include regular_user_interface urls in root

--- soya/urls.py
+++ soya/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^$', 'soya.views.home_redirect', name='home_redirect'),
     url(r'^accounts/', include('authentification.urls')),
     url(r'^administration/', include('administration.urls')),
+    url(r'^films/', include('regular_users_interface.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
