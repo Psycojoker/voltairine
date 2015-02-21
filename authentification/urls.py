6f9bf8e231b7308e8fe10d96615be5af17bcf7f6
Laurent Peuch <cortex@worlddomination.be>
2015-02-21 20:07:58 +0100

    [mod] redirect to login page on logout

--- authentification/urls.py
+++ authentification/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('authentification.views',
     url(r'^login/', 'login', name='login'),
+    url(r'^logout/', 'logout', name='logout'),
     url(r'^', include('django.contrib.auth.urls')),
 )
--- authentification/views.py
+++ authentification/views.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.views import login as base_login
+from django.contrib.auth import logout as auth_logout
+from django.http import HttpResponseRedirect
 
 
 def login(request):
     return base_login(request, template_name="registration/login.haml")
+
+
+def logout(request):
+    auth_logout(request)
+    return HttpResponseRedirect("/")
