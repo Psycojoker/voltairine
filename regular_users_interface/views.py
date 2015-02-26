04b46d2197aea349b3fe655fb13c7e39149622de
Laurent Peuch <cortex@worlddomination.be>
2015-02-26 07:45:04 +0100

    [enh] send the video the user can see on the page

--- regular_users_interface/views.py
+++ regular_users_interface/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from video.models import Video
+
 
 def dashboard(request):
-    return render(request, 'regular_users_interface/dashboard.haml', {})
+    return render(request, 'regular_users_interface/dashboard.haml', {
+        Video.objects.filter(videosection__subsubsection__permission__user=request.user),
+    })
