2c2d29799ce176765d4bea7b2f7d9dcca61744ec
Laurent Peuch <cortex@worlddomination.be>
2015-02-26 09:21:02 +0100

    [enh] only logged-in users can see video

--- regular_users_interface/urls.py
+++ regular_users_interface/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 from django.views.generic import DetailView
+from django.contrib.auth.decorators import login_required
 
 from video.models import Video
 
 
 urlpatterns = patterns('regular_users_interface.views',
     url(r'^$', 'dashboard', name='user_dashboard'),
-    url(r'^(?P<pk>\d+)/$', DetailView.as_view(model=Video, template_name="regular_users_interface/video_detail.haml"), name='user_video_detail'),
+    url(r'^(?P<pk>\d+)/$', login_required(DetailView.as_view(model=Video, template_name="regular_users_interface/video_detail.haml")), name='user_video_detail'),
 )
