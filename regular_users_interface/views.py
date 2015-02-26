7dc2417eec50a7fe6206a494305792cf7ed4c39f
Laurent Peuch <cortex@worlddomination.be>
2015-02-26 07:48:41 +0100

    [enh] user dashboard can only be accessed by authentificated users

--- regular_users_interface/views.py
+++ regular_users_interface/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from video.models import Video
 
 
+@login_required
 def dashboard(request):
     return render(request, 'regular_users_interface/dashboard.haml', {
         Video.objects.filter(videosection__subsubsection__permission__user=request.user),
