020ba8845e532346f1aa74704b2059307a125af9
Laurent Peuch <cortex@worlddomination.be>
2015-02-26 09:06:51 +0100

    [enh] list all the videos the user has access too

--- regular_users_interface/views.py
+++ regular_users_interface/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from video.models import Video
+from sections.models import SubSection
 
 
 @login_required
 def dashboard(request):
     return render(request, 'regular_users_interface/dashboard.haml', {
-        "video_list": Video.objects.filter(videosection__subsubsection__permission__user=request.user),
+        "subsection_list": SubSection.objects.filter(subsubsection__permission__user=request.user).distinct(),
     })
