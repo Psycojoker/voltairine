a07f3f4d71d7bacb89559bcb7b460d4ad993e11e
Laurent Peuch <cortex@worlddomination.be>
2015-02-26 09:20:23 +0100

    [enh] link to detail page for user video

--- regular_users_interface/urls.py
+++ regular_users_interface/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, url
+from django.views.generic import DetailView
+
+from video.models import Video
 
 
 urlpatterns = patterns('regular_users_interface.views',
     url(r'^$', 'dashboard', name='user_dashboard'),
+    url(r'^(?P<pk>\d+)/$', DetailView.as_view(model=Video, template_name="regular_users_interface/video_detail.haml"), name='user_video_detail'),
 )
