54a798ec00ad1200bb0aad7dd8416d9ea895f0d7
Laurent Peuch <cortex@worlddomination.be>
2015-03-01 03:03:48 +0100

    [enh] make working editing form

--- administration/static/js/video_detail.js
+++ administration/static/js/video_detail.js
@@ -1,7 +1,15 @@
 function editVideoDetailsController($scope) {
     $scope.inEditMode = false;
 
-    $scope.switchToEditMode = function() {
-        $scope.inEditMode = true;
+    $scope.toggleEditMode = function() {
+        $scope.inEditMode = !$scope.inEditMode;
+        if ($scope.inEditMode == false) {
+            $scope.title = $("#id_title").val();
+            $scope.film_name = $("#id_film_name").val();
+            $scope.realisation = $("#id_realisation").val();
+            $scope.production = $("#id_production").val();
+            $scope.photo_direction = $("#id_photo_direction").val();
+            $scope.observations = $("#id_observations").val();
+        }
     }
 }
