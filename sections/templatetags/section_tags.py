8d901d0b7489d4db89f3d93f0f860782199550fd
Laurent Peuch <cortex@worlddomination.be>
2015-03-14 17:11:48 +0100

    [enh] redisplay permission switch on user detail page

--- sections/templatetags/section_tags.py
+++ sections/templatetags/section_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 
 @register.simple_tag
-def is_user_have_access(subsubsection, user):
-    if Permission.objects.filter(user=user, subsubsection=subsubsection).exists():
+def is_user_have_access(section, user):
+    if Permission.objects.filter(user=user, section=section).exists():
         return "true"
     return "false"
