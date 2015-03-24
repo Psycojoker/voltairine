4da35d1637838fed2644ece9acfa1a9387a4145e
Laurent Peuch <cortex@worlddomination.be>
2015-03-25 00:13:45 +0100

    [fix] admins and users aren't required anymore on creation of group

--- permissions_groups/models.py
+++ permissions_groups/models.py
@@ -7,7 +7,7 @@ from sections.models import Section
 class Group(models.Model):
     name = models.CharField(max_length=255, verbose_name="Nom")
 
-    admins = models.ManyToManyField(User, related_name="group_is_admin_set", verbose_name="Administrateurs")
-    users = models.ManyToManyField(User, related_name="group_is_member_set", verbose_name="Utilisateurs")
+    admins = models.ManyToManyField(User, related_name="group_is_admin_set", verbose_name="Administrateurs", blank=True)
+    users = models.ManyToManyField(User, related_name="group_is_member_set", verbose_name="Utilisateurs", blank=True)
 
     permissions = models.ManyToManyField(Section)
