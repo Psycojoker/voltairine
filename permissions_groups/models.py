5c6fc20c010cdb59a6814d4505666ea3c21918b9
Laurent Peuch <cortex@worlddomination.be>
2015-03-24 16:47:30 +0100

    [enh] add a new Group model for handling permissions and admins

--- permissions_groups/models.py
+++ permissions_groups/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from sections.models import Section
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+
+    admins = models.ManyToManyField(User, related_name="group_is_admin_set")
+    users = models.ManyToManyField(User, related_name="group_is_member_set")
+
+    permissions = models.ManyToManyField(Section)
