e08a16c4aa682e7976be0bb4eced9941be0fa833
Laurent Peuch <cortex@worlddomination.be>
2015-03-24 02:56:23 +0100

    [mod] move code in the code location

--- regular_users_interface/views.py
+++ regular_users_interface/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
 
-from sections.models import Section
+from video.models import Video
+from sections.models import Section, Permission
 
 
 @login_required
@@ -9,3 +12,16 @@ def dashboard(request):
     return render(request, 'regular_users_interface/dashboard.haml', {
         "subsection_list": Section.objects.filter(subsubsection__permission__user=request.user).distinct(),
     })
+
+
+class UserVideoDetail(DetailView):
+    model = Video
+    template_name = "regular_users_interface/video_detail.haml"
+
+    def get(self, request, *args, **kwargs):
+        to_return = super(UserVideoDetail, self).get(request, *args, **kwargs)
+
+        if not Permission.objects.filter(user=request.user, subsubsection__videosection__video=self.object).exists():
+            raise PermissionDenied()
+
+        return to_return
