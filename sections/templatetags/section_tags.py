3db7e2952da7bff094cd2051b1d988f9549fa5de
Laurent Peuch <cortex@worlddomination.be>
2015-03-25 00:21:45 +0100

    [enh] displaying group permissions works

--- sections/templatetags/section_tags.py
+++ sections/templatetags/section_tags.py
@@ -1,6 +1,7 @@
 from django import template
 
 from sections.models import Permission
+from permissions_groups.models import Group
 
 
 register = template.Library()
@@ -9,6 +10,13 @@ register = template.Library()
 @register.simple_tag
 def is_user_have_access(section, user):
     if Permission.objects.filter(user=user, section=section).exists():
+        return "true"
+    return "false"
+
+
+@register.simple_tag
+def is_group_have_access(section, group):
+    if Group.objects.filter(pk=group.pk, permissions=section).exists():
         return "true"
     return "false"
 
