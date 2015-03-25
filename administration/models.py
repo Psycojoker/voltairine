bfd077e65706b9d93cf5e9957cd9822d8d2f6b98
Laurent Peuch <cortex@worlddomination.be>
2015-03-25 04:43:57 +0100

    [enh] display user the same way everywhere

--- administration/models.py
+++ administration/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+def render_user(self):
+    if self.first_name and self.last_name:
+        return "%s %s (%s) <%s>%s" % (self.first_name, self.last_name, self.username, self.email, (" - administrateur" if self.is_staff else ""))
+
+    return "%s <%s>%s" % (self.username, self.email, (" - administrateur" if self.is_staff else ""))
+
+
+User.__unicode__ = render_user
