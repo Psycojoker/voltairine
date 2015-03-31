10dae163801e4d87092d693e4924e7b9c386ec9e
Laurent Peuch <cortex@worlddomination.be>
2015-03-31 13:01:07 +0200

    [fix] unauthenticated user don't have a group_is_admin_set related manager

--- soya/views.py
+++ soya/views.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 def home_redirect(request):
     if request.user.is_staff:
         return HttpResponseRedirect(reverse('administration_dashboard'))
-    if request.user.group_is_admin_set.exists():
+    if request.user.is_authenticated() and request.user.group_is_admin_set.exists():
         return HttpResponseRedirect(reverse('administration_dashboard'))
     if request.user.is_authenticated():
         return HttpResponseRedirect(reverse('user_dashboard'))
