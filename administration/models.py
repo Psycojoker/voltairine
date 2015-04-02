8e0902fde19b5f3da862efb08f141cd8e4bb1d74
Laurent Peuch <cortex@worlddomination.be>
2015-04-02 12:57:10 +0200

    [enh] new helper method on user model

--- administration/models.py
+++ administration/models.py
@@ -1,6 +1,8 @@
 from django.db.models import Q
 from django.contrib.auth.models import User
 
+from sections.models import Section
+from sections.utils import unfold_tree
 
 def render_user(self):
     if self.first_name and self.last_name:
@@ -16,5 +18,15 @@ def users_can_administrate(self):
     return User.objects.filter(Q(group_is_admin_set__in=groups_managed_by_user)|Q(group_is_member_set__in=groups_managed_by_user))
 
 
+def sections_can_administrate(self):
+    sections_of_groups = Section.objects.filter(group__admins=self)
+
+    sections_tree = Section.objects.all().as_python_tree()
+    node_to_childrens = unfold_tree(sections_tree)
+
+    return set(list(sections_of_groups) + sum([node_to_childrens[x] for x in sections_of_groups], []))
+
+
 User.users_can_administrate = users_can_administrate
 User.groups_managed_by_user = lambda self: self.group_is_admin_set.all()
+User.sections_can_administrate = sections_can_administrate
