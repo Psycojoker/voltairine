4283aaf601482ee2512c642101f587ffe3515ef9
Laurent Peuch <cortex@worlddomination.be>
2015-05-22 04:03:55 +0200

    [enh] raise if user doesn't exist in forgotten password form

--- authentification/forms.py
+++ authentification/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 
+from django.contrib.auth.models import User
+
 
 class ForgottenPasswordForm(forms.Form):
     username = forms.CharField(label="Identifiant")
     email = forms.EmailField(label="Votre adresse e-mail")
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+
+        if not User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Cet utilisateur n'existe pas")
+
+        return username
