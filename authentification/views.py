f63e7ebf0e85a8f32685a0ff4266950682324a7e
Laurent Peuch <cortex@worlddomination.be>
2015-05-22 03:59:36 +0200

    [enh] send email for not working password

--- authentification/views.py
+++ authentification/views.py
@@ -1,8 +1,14 @@
+# encoding: utf-8
+
 from django.contrib.auth.views import login as base_login
 from django.contrib.auth import logout as auth_logout
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
+from django.template.loader import get_template
+from django.template import Context
 
 from .forms import ForgottenPasswordForm
 
@@ -25,6 +31,12 @@ def forgotten_password(request):
     if not form.is_valid():
         return render(request, 'registration/forgotten_password.haml', {"form": form})
 
-    #TODO email
+    send_mail(
+              u'Demande de mot de pass oublié par %s pour %s' % (form.cleaned_data['email'], form.cleaned_data['username']),
+              get_template('registration/forgotten_password_email.txt').render(Context(form.cleaned_data)),
+              'noreply@play.saya.fr',
+              settings.SAYA_FORGOTTEN_PASSWORD_EMAILS,
+              fail_silently=False
+    )
 
     return HttpResponseRedirect(reverse('forgotten_password_success'))
