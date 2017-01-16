742a56c111670f0a9797514856796f0bf3fd6503
Laurent Peuch <cortex@worlddomination.be>
2017-01-16 09:01:09 +0100

    [enh] add video to admin if

--- video/admin.py
+++ video/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Video
 
-# Register your models here.
+
+class VideoAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Video, VideoAdmin)
