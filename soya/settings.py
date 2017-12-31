71d69855bc0120be8fa77038db05174c42580891
Laurent Peuch <cortex@worlddomination.be>
2017-12-31 09:40:51 +0100

    [enh] uses AUTH_PASSWORD_VALIDATORS to increase password strengh

--- soya/settings.py
+++ soya/settings.py
@@ -110,6 +110,21 @@ TEMPLATES = [{
     }
 }]
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
