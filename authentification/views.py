a74c3653e32b01d6fa658d6a226144fd8196caa8
Laurent Peuch <cortex@worlddomination.be>
2018-03-09 18:19:40 +0100

    [mod] remove deadcode

--- authentification/views.py
+++ authentification/views.py
@@ -20,24 +20,3 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return HttpResponseRedirect("/")
-
-
-# XXX this is not used anymore
-def forgotten_password(request):
-    if request.method == 'GET':
-        return render(request, 'registration/forgotten_password.haml', {"form": ForgottenPasswordForm()})
-
-    form = ForgottenPasswordForm(request.POST)
-
-    if not form.is_valid():
-        return render(request, 'registration/forgotten_password.haml', {"form": form})
-
-    send_mail(
-              u'Demande de mot de passe oublié par %s pour %s' % (form.cleaned_data['email'], form.cleaned_data['username']),
-              get_template('registration/forgotten_password_email.txt').render(Context(form.cleaned_data)),
-              'noreply@play.saya.fr',
-              settings.SAYA_FORGOTTEN_PASSWORD_EMAILS,
-              fail_silently=False
-    )
-
-    return HttpResponseRedirect(reverse('forgotten_password_success'))
